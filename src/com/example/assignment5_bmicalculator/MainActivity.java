18aebd0aa39a6b3b6a626b1cdf6b0d1176ecfcea
MrCiprian89 <mr.ciprian89@gmail.com>
2015-03-31 18:54:36 -0400

    Created functionality for units button, will change the text of the button to show which units are being used for the calculation as well as store a boolean value to use the correct BMI formula

--- src/com/example/assignment5_bmicalculator/MainActivity.java
+++ src/com/example/assignment5_bmicalculator/MainActivity.java
@@ -1,18 +1,63 @@
 package com.example.assignment5_bmicalculator;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	private int _myHeight;
+	private int _myWeight;
+	private double BMI;
+	//is true if metric , false if imperial
+	private boolean metricOrImperial = true;
+	private EditText _myHeightEditText;
+	private EditText _myWeightEditText;
+	private EditText _BMIEditText;
+	Button unitsButton;
+	Button calculateButton;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		 //create a reference to my Controls
+        this._myHeightEditText = (EditText) findViewById(R.id.editHeight);
+        this._myWeightEditText = (EditText) findViewById(R.id.editWidth);
+        this._BMIEditText = (EditText) findViewById(R.id.BMIeditText);		
+        unitsButton = (Button) findViewById(R.id.changeUnitsButton);
+        calculateButton = (Button) findViewById(R.id.calculateButton);
+        
+        //make an event that changes the text of the units button and sets the calculations of BMI units
+        OnClickListener oclUnitsButton = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	if (metricOrImperial){
+            	unitsButton.setText("Imperial Units");
+            	metricOrImperial = false;
+            }
+            	else{
+            		unitsButton.setText("Metric Units");
+                	metricOrImperial = true;
+            	}
+            }
+          };
+        
+          // assign click listener to the OK button (btnOK)
+          unitsButton.setOnClickListener(oclUnitsButton);
+          	
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
